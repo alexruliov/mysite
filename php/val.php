9d37ca9d3dca29e5a0d8c1510f06dd4a399c0158
Ruliov <1494022ar>
2015-09-15 10:30:39 +0300

    ajax все работает

--- php/val.php
+++ php/val.php
@@ -5,21 +5,11 @@ class Validator {
     public function prov (){
             if (! (filter_has_var(INPUT_POST, 'name') &&
             (strlen(filter_input(INPUT_POST, 'name')) > 0))) {
-            return false;}
-        else return true;
+                echo '<p style="color: red">Заполните имя</p>';}
+        else echo '<p style="color: green">Все впорядке</p>';}
         }
-    public function ret (){
 
-        $a = new Validator();
-        $d = $a->prov();
-        if ($d == true){
-            echo json_encode('Ok');}
-        if ($d == false){
-            echo json_encode('Error');}
-
-    }
-    }
 $c = new Validator();
-$c->ret();
+$c->prov();
 
 
